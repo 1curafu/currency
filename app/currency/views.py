f967765e4d3c2e2004d1442a8661a4da57fcd939
1curafu <icurafu333@icloud.com>
2023-02-24 10:04:26 +0100

    Homework 4

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,25 +1,23 @@
-from django.http import HttpResponse
 
 from currency.models import Rate, ContactUs
+from django.shortcuts import render
 
 
 def list_rates(request):
-    qs = Rate.objects.all()
+    rates = Rate.objects.all()
 
-    result = []
-    for rate in qs:
-        result.append(f'id: {rate.id}, buy: {rate.buy}, sell: {rate.sell}, currency: {rate.currency}, '
-                      f'source: {rate.source}, created: {rate.created} <br>')
+    context = {
+        "rates": rates
+    }
 
-    return HttpResponse(str(result))
+    return render(request, 'rates_list.html', context)
 
 
 def list_contacts(request):
-    qs = ContactUs.objects.all()
+    contacts = ContactUs.objects.all()
 
-    result = []
-    for contact in qs:
-        result.append(f'id: {contact.id}, email: {contact.email_from}, subject: {contact.subject}, '
-                      f'message: {contact.message} <br>')
+    context = {
+        "contacts": contacts
+    }
 
-    return HttpResponse(str(result))
+    return render(request, 'contactus.html', context)
